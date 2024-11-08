107e7625b22e9f3be34b2c5156e69fbc9ba38fa9
Ian Calica <Iggc654@gmail.com>
2024-11-08 23:51:08 +0800

    updated

--- my_core_project/deploymentSettings.py
+++ my_core_project/deploymentSettings.py
@@ -32,9 +32,7 @@ STORAGES = {
 }
 
 DATABASES = {
-    "default": {
-        dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"), conn_max_age=600
-        ),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"), conn_max_age=600
+    )
 }
